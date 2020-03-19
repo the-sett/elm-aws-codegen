f410fc7f844762ba7493c9449d60aca4ad3eb023
Rupert Smith <rupert@thesett.com>
2020-03-19 12:24:52 +0000

    Added more services covering all protocols and some with known errors.

--- src/index.js
+++ src/index.js
@@ -49,19 +49,22 @@ ${formatLocations.join('\n')}`);
 // });
 
 const specs = [
-  // "athena-2017-05-18.normal.json",
-  // "batch-2016-08-10.normal.json",
-  // "cloudformation-2010-05-15.normal.json",
-  // "cloudtrail-2013-11-01.normal.json",
+  "athena-2017-05-18.normal.json",
+  "batch-2016-08-10.normal.json",
+  "cloudformation-2010-05-15.normal.json",
+  "cloudfront-2019-03-26.normal.json",
+  "cloudtrail-2013-11-01.normal.json",
   "cognito-identity-2014-06-30.normal.json",
-  "cognito-idp-2016-04-18.normal.json"
-  // "dynamodb-2012-08-10.normal.json",
-  // "ec2-2016-11-15.normal.json",
-  // "iam-2010-05-08.normal.json",
-  // "lambda-2015-03-31.normal.json",
-  // "rds-2014-10-31.normal.json",
-  // "route53-2013-04-01.normal.json",
-  // "s3-2006-03-01.normal.json"
+  "cognito-idp-2016-04-18.normal.json",
+  "dynamodb-2012-08-10.normal.json",
+  "ec2-2016-11-15.normal.json",
+  "extra-2018-08-02.normal.json",
+  "iam-2010-05-08.normal.json",
+  "iot-2015-05-28.normal.json",
+  "lambda-2015-03-31.normal.json",
+  "rds-2014-10-31.normal.json",
+  "route53-2013-04-01.normal.json",
+  "s3-2006-03-01.normal.json"
 ]
 
 specs.forEach(function(item, index) {
