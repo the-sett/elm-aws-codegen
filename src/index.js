e486f4145e87fe0f4f836b403e0e4033316ff6a8
Rupert Smith <rupert@thesett.com>
2024-12-16 12:10:08 +0000

    Adding an elm.json for building the stubs.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 var fs = require('fs');
 var glob = require('glob');
 const path = require('path');
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 
-const { Elm } = require('./elm.js');
+const {Elm} = require('./elm.js');
 const app = Elm.Top.init();
 
 //---=== Find and run elm-format.
 
 const formatLocations = [
-  // Running locally during dev of this package
-  path.join(__dirname, '..', 'node_modules', '.bin', 'elm-format'),
+    // Running locally during dev of this package
+    path.join(__dirname, '..', 'node_modules', '.bin', 'elm-format'),
 
-  // Installed in the npm .bin folder.
-  // webpack-dev-server should be along side it
-  path.join(__dirname, 'elm-format'),
+    // Installed in the npm .bin folder.
+    // webpack-dev-server should be along side it
+    path.join(__dirname, 'elm-format'),
 ];
 
 const findFormat = () => formatLocations
-  .filter(fs.existsSync)[0];
+    .filter(fs.existsSync)[0];
 
 const formatFile = (filename) => {
-  const format = findFormat();
+    const format = findFormat();
 
-  if (!format) {
-    console.error(`Unable to find elm-format among:
+    if (!format) {
+        console.error(`Unable to find elm-format among:
 ${formatLocations.join('\n')}`);
-    return;
-  }
+        return;
+    }
 
-  spawn(format, [filename, '--yes'], {
-    cwd: process.cwd(),
-    env: process.env,
-    stdio: ['ignore', 'ignore', process.stderr],
-  });
+    spawn(format, [filename, '--yes'], {
+        cwd: process.cwd(),
+        env: process.env,
+        stdio: ['ignore', 'ignore', process.stderr],
+    });
 };
 
 //---=== Find API specs and process them.
@@ -49,66 +49,72 @@ ${formatLocations.join('\n')}`);
 // });
 
 const specs = [
-  "athena-2017-05-18.normal.json",
-  "batch-2016-08-10.normal.json",
-  "ce-2017-10-25.normal.json",
-  "cloudformation-2010-05-15.normal.json",
-  "cloudfront-2019-03-26.normal.json",
-  "cloudtrail-2013-11-01.normal.json",
-  "cognito-identity-2014-06-30.normal.json",
-  "cognito-idp-2016-04-18.normal.json",
-  "dynamodb-2012-08-10.normal.json",
-  "ec2-2016-11-15.normal.json",
-  "ecr-2015-09-21.normal.json",
-  "ecs-2014-11-13.normal.json",
-  "eventbridge-2015-10-07.normal.json",
-  "extra-2018-08-02.normal.json",
-  "iam-2010-05-08.normal.json",
-  "lambda-2015-03-31.normal.json",
-  "rds-2014-10-31.normal.json",
-  "route53-2013-04-01.normal.json",
-  "s3-2006-03-01.normal.json",
-  "sns-2010-03-31.normal.json",
-  "sqs-2012-11-05.normal.json",
-  "iotevents-2018-07-27.normal.json",
-  "iot1click-devices-2018-05-14.normal.json",
-  "iotthingsgraph-2018-09-06.normal.json",
-  "iotevents-data-2018-10-23.normal.json",
-  "iot-jobs-data-2017-09-29.normal.json",
-  "iot1click-projects-2018-05-14.normal.json",
-  "iot-2015-05-28.normal.json",
-  "iotanalytics-2017-11-27.normal.json",
-  "iot-data-2015-05-28.normal.json"
+    "athena-2017-05-18.normal.json",
+    "batch-2016-08-10.normal.json",
+    "ce-2017-10-25.normal.json",
+    "cloudformation-2010-05-15.normal.json",
+    "cloudfront-2019-03-26.normal.json",
+    "cloudtrail-2013-11-01.normal.json",
+    "cognito-identity-2014-06-30.normal.json",
+    "cognito-idp-2016-04-18.normal.json",
+    "dynamodb-2012-08-10.normal.json",
+    "ec2-2016-11-15.normal.json",
+    "ecr-2015-09-21.normal.json",
+    "ecs-2014-11-13.normal.json",
+    "eventbridge-2015-10-07.normal.json",
+    "extra-2018-08-02.normal.json",
+    "iam-2010-05-08.normal.json",
+    "lambda-2015-03-31.normal.json",
+    "rds-2014-10-31.normal.json",
+    "route53-2013-04-01.normal.json",
+    "s3-2006-03-01.normal.json",
+    "sns-2010-03-31.normal.json",
+    "sqs-2012-11-05.normal.json",
+    "iotevents-2018-07-27.normal.json",
+    "iot1click-devices-2018-05-14.normal.json",
+    "iotthingsgraph-2018-09-06.normal.json",
+    "iotevents-data-2018-10-23.normal.json",
+    "iot-jobs-data-2017-09-29.normal.json",
+    "iot1click-projects-2018-05-14.normal.json",
+    "iot-2015-05-28.normal.json",
+    "iotanalytics-2017-11-27.normal.json",
+    "iot-data-2015-05-28.normal.json"
 ]
 
-specs.forEach(function(item, index) {
-  var filename = 'api/' + item;
+specs.forEach(function (item, index) {
+    var filename = 'api/' + item;
 
-  fs.readFile(filename, 'utf8', function(err, contents) {
-    app.ports.modelInPort.send([filename, contents]);
-  });
+    fs.readFile(filename, 'utf8', function (err, contents) {
+        app.ports.modelInPort.send([filename, contents]);
+    });
 });
 
 app.ports.codeOutPort.subscribe(request => {
-  console.log("=== Processed ===: " + request[0] + "\n");
+    console.log("=== Processed ===: " + request[0] + "\n");
 
-  request[2].forEach(function(item, index) {
-    console.log(item + "\n");
-  });
+    request[2].forEach(function (item, index) {
+        console.log(item + "\n");
+    });
 
-  var filename = 'stubs/AWS/' + request[0];
+    const dir = "stubs/src/AWS";
 
-  fs.writeFile(filename, request[1], (err) => {
-    if (err) throw err;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
 
-    formatFile(filename);
-  })
+    var filename = 'stubs/src/AWS/' + request[0];
+
+    fs.writeFile(filename, request[1], (err) => {
+        if (err) throw err;
+
+        formatFile(filename);
+    })
 });
 
 app.ports.errorOutPort.subscribe(request => {
-  console.log("=== Errors While Processing ========= " + request[0] + "\n");
+    console.log("=== Errors While Processing ========= " + request[0] + "\n");
 
-  request[1].forEach(function(item, index) {
-    console.log(item + "\n");
-  });
+    request[1].forEach(function (item, index) {
+        console.log(item + "\n");
+    });
 });
